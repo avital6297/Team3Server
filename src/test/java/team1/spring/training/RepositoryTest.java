99a03349964127be8fc5f63981ed19c5fb42608b
MASSMASS <avital6297@gmail.com>
2020-03-24 17:04:55 +0200

    Create JUnit test class for repository

--- src/test/java/team1/spring/training/RepositoryTest.java
+++ src/test/java/team1/spring/training/RepositoryTest.java
@@ -10,6 +10,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
@@ -20,11 +22,58 @@ public class RepositoryTest {
     private TestEntityManager entityManager;
 
     @Autowired
-    private CrudRepository<?,?> repository;
+    private CrudRepository<File,Long> repository;
+
+    @Autowired
+    private FileRepository fileRepository;
+
+    String fileName = "fileName";
+    String fileLoction = "C:\\Uploads";
+    String newFileName = "newFileName";
+    File file = new File(fileLoction, new java.util.Date().toString(),fileName);
+
 
     @Test
-    public void test() {
+    public void testDelete() {
+        final Long id = entityManager.persistAndGetId(file, Long.class);
+        repository.delete(id);
+        entityManager.flush();
+        File after = entityManager.find(File.class, id);
+        assertEquals(null,after);
+    }
 
+    @Test
+    public void testCreate() {
+        final Long id = entityManager.persistAndGetId(file, Long.class);
+        saveFile(file);
+        File after = entityManager.find(File.class, id);
+        assertEquals(after, file);
+    }
 
+    @Test
+    public void testUpdate() {
+        final Long id = entityManager.persistAndGetId(file, Long.class);
+        saveFile(file);
+
+        file.setName(newFileName);
+        saveFile(file);
+        File after = entityManager.find(File.class, id);
+        assertEquals(after.getName(), newFileName);
+    }
+
+    @Test
+    public void testFindByName() {
+        saveFile(file);
+        saveFile(file);
+
+        List<File> files = fileRepository.findByName(fileName);
+        for(int i=0; i< files.size(); i++){
+            assertEquals(files.get(i).getName(), fileName);
+        }
+    }
+
+    private void saveFile(File file) {
+        repository.save(file);
+        entityManager.flush();
     }
 }
