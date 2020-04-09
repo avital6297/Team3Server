21eabe6ca66612299d3d8e21bdccdf5c89a659bd
MASSMASS <avital6297@gmail.com>
2020-04-09 22:41:14 +0300

    Add JUnit test for list all files using MockMvc

---
+++ src/main/java/team1/spring/training/MyWebConfig.java
@@ -0,0 +1,28 @@
+package team1.spring.training;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan({ "test.controllers" })
+public class MyWebConfig extends WebMvcConfigurationSupport {
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        return multipartResolver;
+    }
+
+//    @Bean
+//    public FileController uploadController() {
+//        return new FileController();
+//    }
+}
+
+
---
+++ src/test/java/team1/spring/training/UploadControllerTest.java
@@ -0,0 +1,40 @@
+package team1.spring.training;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebAppConfiguration
+@ContextConfiguration(classes = MyWebConfig.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UploadControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Test
+    public void test() throws Exception {
+        MockMultipartFile file = new MockMultipartFile("file", "filename.txt", "text/plain", "some xml".getBytes());
+
+        MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        mockMvc.perform(MockMvcRequestBuilders.fileUpload("/user/file")
+                .file(file)
+                .accept(MediaType.APPLICATION_JSON));
+    }
+}
+
+
